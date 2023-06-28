6e53da129fff310138790520e08f4a2d818d995b
Prakashgolusingh <95623466+Prakashgolusingh@users.noreply.github.com>
2023-06-28 18:57:15 +0000

    added css using bootsrap

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+jobs=[
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bangluru',
+    'salary':245_0000
+  },
+  {
+    'id':2,
+    'title':'SWE',
+    'location':'Chennai',
+    'salary':343_0000
+  },
+  {
+    'id':3,
+    'title':'Full Stack Developer',
+    'location':'Noida',
+    'salary':200_0000
+  }
+]
+
 @app.route('/')
 def home():
-  return "Hello World!"
+  return render_template('home.html',jobs=jobs, company_name='Jovian')
+
+
 
 if( __name__ == '__main__'):
   app.run(host ='0.0.0.0', debug=True)
